1e20a81f225b50ffbd37fb9951ca996c2a4b58cb
Moses <moses.junior@student.moringaschool.com>
2021-10-16 20:00:21 +0300

    implementing a sign up form

--- gram/urls.py
+++ gram/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
+    path('users/',include('users.urls')),
     path('',views.welcome,name = 'welcome'),
 
 ]
